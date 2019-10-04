18480e185cb81d71f285744747e08f4c29536c2b
Jonathan Norman <jnorman2123@gmail.com>
2019-10-04 05:57:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   data.each do |attribute_name|
-    attribute_name.each do |attribute|
+    attribute_name.each do |attribute, value|
       
     
       binding.pry 
