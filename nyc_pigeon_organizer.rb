6b47a8ec37356992ec014f31c58a5f01258eb803
Jonathan Norman <jnorman2123@gmail.com>
2019-10-04 06:02:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,8 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   name_hash = {}
-  data.each do |attribute_name|
-    attribute_name.each do |attribute|
+  data.each do |attribute|
       
-    
       binding.pry 
-    end
-    
   end 
 end
